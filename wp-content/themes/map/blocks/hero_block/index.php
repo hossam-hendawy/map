80d222307175fb7375405b8cc97c7d9f2d2e2867
Hossam <hossam@digihive.dev>
2025-01-25 07:19:30 +0200

    feat : add cta button style and links

--- wp-content/themes/map/blocks/hero_block/index.php
+++ wp-content/themes/map/blocks/hero_block/index.php
@@ -2,98 +2,81 @@
 $id = '';
 $className = 'hero_block';
 if (isset($block)) {
-    $id = $block['id'];
-    if (!empty($block['anchor'])) {
-        $id = $block['anchor'];
+  $id = $block['id'];
+  if (!empty($block['anchor'])) {
+    $id = $block['anchor'];
+  }
+  if (!empty($block['className'])) {
+    $className .= ' ' . $block['className'];
+  }
+  if (!empty($block['align'])) {
+    $className .= ' align' . $block['align'];
+  }
+  if (function_exists('is_admin') && is_admin()) {
+    if (wp_is_json_request() || (defined('REST_REQUEST') && REST_REQUEST) || (function_exists('get_current_screen') && get_current_screen()->is_block_editor())) {
+      echo '<img width="100%" height="100%" src="' . get_template_directory_uri() . '/blocks/hero_block/screenshot.png">';
+      return;
     }
-    if (!empty($block['className'])) {
-        $className .= ' ' . $block['className'];
-    }
-    if (!empty($block['align'])) {
-        $className .= ' align' . $block['align'];
-    }
-    if (function_exists('is_admin') && is_admin()) {
-        if (wp_is_json_request() || (defined('REST_REQUEST') && REST_REQUEST) || (function_exists('get_current_screen') && get_current_screen()->is_block_editor())) {
-            echo '<img width="100%" height="100%" src="' . get_template_directory_uri() . '/blocks/hero_block/screenshot.png">';
-            return;
-        }
-    }
+  }
 }
 ?>
 <?php
-$image = get_field('image');
-$right_image = get_field('right_image');
-$trustpilot_rating = get_field('trustpilot_rating');
-$title = get_field('title');
-$description = get_field('description');
-$first_cta_button = get_field('first_cta_button');
-$second_cta_button = get_field('second_cta_button');
 
 ?>
 <section id="<?= esc_attr($id) ?>" class="<?= esc_attr($className) ?>">
-    <div class="images-animation-wrapper">
-        <?php
-        $picture_class = 'cover-image';
-        echo bis_get_attachment_picture(
-            $image,
-            [
-                375 => [375, 544, 1],
-                1024 => [1280, 630, 1],
-                1280 => [1280, 630, 1],
-                1440 => [1440, 630, 1],
-            ],
-            [
-                'retina' => true, 'picture_class' => $picture_class,
-            ]
-        );
-        ?>
-        <?php
-        $picture_class = 'right-cover-image';
-        echo bis_get_attachment_picture(
-            $right_image,
-            [
-                375 => [375, 544, 1],
-                1024 => [500, 630, 1],
-                1280 => [500, 630, 1],
-                1440 => [500, 630, 1],
-            ],
-            [
-                'retina' => true, 'picture_class' => $picture_class,
-            ]
-        );
-        ?>
-    </div>
-    <div class="rectangle rectangle-1"></div>
-    <div class="rectangle rectangle-2"></div>
-    <div class="container">
-        <div class="content-wrapper">
-            <?php if ($trustpilot_rating): ?>
-                <div class="trustpilot-logo-and-text">
-                    <div class="body-2 white-color">
-                        <?= $trustpilot_rating ?>
-                    </div>
-                    <picture class="trustpilot-logo">
-                        <img src=" <?= get_template_directory_uri() . '/images/trustpilot.png' ?>" alt="trustpilot">
-                    </picture>
-                </div>
-            <?php endif; ?>
-            <?php if ($title): ?>
-                <h1 class="en-h1 title white-color"><?= $title ?></h1>
-            <?php endif; ?>
-            <?php if ($description): ?>
-                <div class="body description white-color"><?= $description ?></div>
-            <?php endif; ?>
-            <div class="cta-buttons">
-                <?php if (!empty($first_cta_button) && is_array($first_cta_button)) { ?>
-                    <a class="cta-button" href="<?= $first_cta_button['url'] ?>" target="<?= $first_cta_button['target'] ?>"><?= $first_cta_button['title'] ?></a>
-                <?php } ?>
-                <?php if (!empty($second_cta_button) && is_array($second_cta_button)) { ?>
-                    <a class="cta-button light-cta" href="<?= $second_cta_button['url'] ?>" target="<?= $second_cta_button['target'] ?>"><?= $second_cta_button['title'] ?></a>
-                <?php } ?>
+  
+  
+  <a href="#" class="cta-button">Map View</a>
+  <br>
+  <br>
+  <br>
+  <a href="#" class="cta-button has-icon">
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" aria-hidden="true">
+      <path d="M22 9V15C22 17.5 21.5 19.25 20.38 20.38L14 14L21.73 6.27C21.91 7.06 22 7.96 22 9Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M21.73 6.27L6.26999 21.73C3.25999 21.04 2 18.96 2 15V9C2 4 4 2 9 2H15C18.96 2 21.04 3.26 21.73 6.27Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M20.38 20.38C19.25 21.5 17.5 22 15 22H9.00003C7.96003 22 7.06002 21.91 6.27002 21.73L14 14L20.38 20.38Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M6.23977 7.98C6.91977 5.05 11.3198 5.05 11.9998 7.98C12.3898 9.7 11.3098 11.16 10.3598 12.06C9.66977 12.72 8.57979 12.72 7.87979 12.06C6.92979 11.16 5.83977 9.7 6.23977 7.98Z" stroke="white" stroke-width="1.5"/>
+      <path d="M9.0946 8.70001H9.10359" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    Map View
+  </a>
+  <br>
+  <br>
+  <br>
+  <a href="#" class="cta-link">
+    View Details
+    <svg viewBox="0 0 41 40" fill="none" aria-hidden="true">
+      <path d="M24.7168 9.8833L34.8335 20L24.7168 30.1166" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M6.50049 20H34.5505" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  </a>
+  <br>
+  <br>
+  <br>
+  <a href="#" class="cta-link small-cta-link">
+    View Details
+    <svg viewBox="0 0 41 40" fill="none" aria-hidden="true">
+      <path d="M24.7168 9.8833L34.8335 20L24.7168 30.1166" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M6.50049 20H34.5505" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  </a>
+  <br>
+  <br>
+  <br>
+  
+  <div class="sssss" style="color: black; text-align: center; "> Performance Food Group Cold Storage Distribution
+    Center
+  </div>
+  <br>
+  <br>
+  <br>
+  <br>
+  <div class="gold">94,000 SF</div>
+  <br>
+  <br>
+  <br>
+  <br>
 
-            </div>
-        </div>
-    </div>
 </section>
 
 
--- wp-content/themes/map/header.php
+++ wp-content/themes/map/header.php
@@ -5,46 +5,11 @@
   <meta content="width=device-width, initial-scale=1.0, maximum-scale=5, minimum-scale=1.0" name="viewport">
   <meta content="ie=edge" http-equiv="X-UA-Compatible">
   <?php wp_head(); ?>
-  <?php
-  $header_banner = get_field('header_banner', 'options');
-  $flag = get_field('flag', 'options');
-  $header_logo = get_field('header_logo', 'options');
-  $contact_number = get_field('contact_number', 'options');
-  $sustainability_link = get_field('sustainability_link', 'options');
-  $contact_us = get_field('contact_us', 'options');
-  
-  ?>
 <body <?php body_class(); ?>>
-<?php if ($header_banner): ?>
-  <div class="header-banner">
-    <?= $header_banner ?>
-    <?php if (!empty($flag) && is_array($flag)) { ?>
-      <picture class="flag-wrapper cover-image">
-        <img src="<?= $flag['url'] ?>" alt="<?= $flag['alt'] ?>">
-      </picture>
-    <?php } ?>
-  </div>
-<?php endif; ?>
 <header>
   <div class="header-wrapper">
     
-    <a href="#" class="cta-link map-h1">
-      View Details
-      <svg  viewBox="0 0 41 40" fill="none" aria-hidden="true">
-        <path d="M24.7168 9.8833L34.8335 20L24.7168 30.1166" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M6.50049 20H34.5505" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-      </svg>
-    
-    </a>
-    <div class="sssss" style="color: black; text-align: center; "> Performance Food Group Cold Storage Distribution
-      Center
-    </div>
-    <br>
-    
-    <div style="color: black; text-align: center; ">94,000 SF</div>
-    <br>
-    
-    <div style="color: black; text-align: center; ">View Details</div>
+   
     
     
     new header
